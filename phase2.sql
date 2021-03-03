0b95acbde0a1790745105cabc57d2e91c1d02706
Charles <13723004+charzho@users.noreply.github.com>
2021-03-02 23:21:19 -0800

    Added participation constraints

--- phase2.sql
+++ phase2.sql
@@ -20,10 +20,8 @@ CREATE TABLE Crew (
 	PRIMARY KEY(crew_ID)
 );
 
-	
-
 CREATE TABLE Customer ( 
-	c_ID INTEGER NOT NULL, 
+	cust_ID INTEGER NOT NULL, 
 	first_name CHAR(30),
 	last_name CHAR(30),
 	gender CHAR(10),
@@ -31,43 +29,39 @@ CREATE TABLE Customer (
 	address CHAR(30),
 	contact_num INTEGER, 
 	zip_code INTEGER,
-	PRIMARY KEY(c_ID)
+	PRIMARY KEY(cust_ID)
 );
-
-
-		       
+	       
 CREATE TABLE Captain (
-	ID INTEGER NOT NULL, 
+	capt_ID INTEGER NOT NULL, 
 	name CHAR(30),
-	PRIMARY KEY(ID)
+	PRIMARY KEY(capt_ID)
 );
 
 CREATE TABLE Ship (
         model CHAR(30),
-        shipID INTEGER NOT NULL,
+        ship_ID INTEGER NOT NULL,
         make CHAR(20),
         age INTEGER,
         seats INTEGER,
-        PRIMARY KEY(shipID)
+        PRIMARY KEY(ship_ID)
 );
- 
-
 
 CREATE TABLE Cruise (
 	c_num INTEGER NOT NULL,
 	cost FLOAT, 
 	num_sold INTEGER,
-	num_stops INTEGER,
+s	num_stops INTEGER,
 	actual_arrive_date CHAR(20),
 	actual_arrive_time CHAR(10),
 	actual_depart_time CHAR(10),
 	source CHAR(30),
 	destination CHAR(30),
-	ID INTEGER,
-	shipID INTEGER,
-	PRIMARY KEY (c_num),
-	FOREIGN KEY (ID) REFERENCES Captain,
-	FOREIGN KEY(shipID) REFERENCES Ship
+	capt_ID INTEGER NOT NULL,
+	ship_ID INTEGER,
+	PRIMARY KEY(c_num),
+	FOREIGN KEY(capt_ID) REFERENCES Captain ON DELETE NO ACTION,
+	FOREIGN KEY(ship_ID) REFERENCES Ship ON DELETE NO ACTION
 	
 );
 
@@ -79,31 +73,28 @@ CREATE TABLE Works (
         FOREIGN KEY(c_num) REFERENCES Cruise
 );
 
-
 CREATE TABLE Schedule ( 
 	day CHAR(10),
 	depart_time CHAR(10),
 	arrive_time CHAR(10),
 	c_num INTEGER NOT NULL,
-	PRIMARY KEY (c_num),
-	FOREIGN KEY (c_num) REFERENCES Cruise
+	PRIMARY KEY(c_num, day),
+	FOREIGN KEY(c_num) REFERENCES Cruise ON DELETE CASCADE
 );
-	
 
 CREATE TABLE Technician (
-	techID INTEGER NOT NULL,
-	PRIMARY KEY(techID)
+	tech_ID INTEGER NOT NULL,
+	PRIMARY KEY(tech_ID)
 );
-
 
 CREATE TABLE repairs (
 	r_date CHAR(10),
 	code INTEGER,
-	shipID INTEGER NOT NULL,
-	techID INTEGER NOT NULL,
-	PRIMARY KEY(shipID, techID),
-	FOREIGN KEY(shipID) REFERENCES Ship,
-	FOREIGN KEY(techID) REFERENCES Technician
+	ship_ID INTEGER NOT NULL,
+	tech_ID INTEGER NOT NULL,
+	PRIMARY KEY(ship_ID, tech_ID),
+	FOREIGN KEY(ship_ID) REFERENCES Ship,
+	FOREIGN KEY(tech_ID) REFERENCES Technician ON DELETE NO ACTION
 );
 
 CREATE TABLE Reservation (
@@ -113,11 +104,11 @@ CREATE TABLE Reservation (
 
 CREATE TABLE has (
 	c_num INTEGER NOT NULL,
-	c_ID INTEGER NOT NULL,
+	cust_ID INTEGER NOT NULL,
 	rnum NUMERIC NOT NULL, 
-	PRIMARY KEY(c_num, c_ID, rnum), 
+	PRIMARY KEY(c_num, cust_ID, rnum), 
 	FOREIGN KEY(c_num) REFERENCES Cruise,
-	FOREIGN KEY(c_ID) REFERENCES Customer,
+	FOREIGN KEY(cust_ID) REFERENCES Customer,
 	FOREIGN KEY(rnum) REFERENCES Reservation
 );
 
@@ -137,12 +128,12 @@ CREATE TABLE Reserved (
 );
 
 CREATE TABLE request (
-	request_id INTEGER NOT NULL,
-	ID INTEGER NOT NULL, 
-	shipID INTEGER NOT NULL, 
-	techID INTEGER NOT NULL, 
-	PRIMARY KEY(request_id, ID, shipID, techID),
-	FOREIGN KEY(ID) REFERENCES Captain,
-	FOREIGN KEY(shipID) REFERENCES Ship,
-	FOREIGN KEY(techID) REFERENCES Technician
+	req_ID INTEGER NOT NULL,
+	capt_ID INTEGER NOT NULL, 
+	ship_ID INTEGER NOT NULL, 
+	tech_ID INTEGER NOT NULL, 
+	PRIMARY KEY(req_ID, capt_ID, ship_ID, tech_ID),
+	FOREIGN KEY(capt_ID) REFERENCES Captain ON DELETE NO ACTION,
+	FOREIGN KEY(ship_ID) REFERENCES Ship,
+	FOREIGN KEY(tech_ID) REFERENCES Technician
 );
